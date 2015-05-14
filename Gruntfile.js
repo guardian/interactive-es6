f438091518bff77fa108b06517c0ccf6cd980bd4
Matt Osborn <matthew.osborn@theguardian.com>
2015-05-14 10:13:16 +0100

    add s3 test/production deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,12 @@
 var fs = require('fs');
-var timestamp = Date.now();
-
-// S3 CONFIG
-// **************************
-var s3bucket = 'gdn-cdn';
-var s3path = undefined; // upload URL e.g. '2015/05/results-interactive/'
-var s3target = 'http://visuals.guim.co.uk/' + s3path + timestamp;
-
-
 
 module.exports = function(grunt) {
 
     require('jit-grunt')(grunt);
 
-    var s3 = grunt.option('s3') || false;
-
-    if (s3 && !s3path) throw new Error('You need to set s3path in Gruntfile.js');
-
-    grunt.log.writeln('Compiling ' + (s3 ? 'for S3' : 'locally'));
-
     grunt.initConfig({
+
+        visuals: { },
 
         watch: {
             js: {
@@ -59,7 +46,7 @@ module.exports = function(grunt) {
 
         shell: {
             interactive: {
-                command: './node_modules/.bin/jspm bundle-sfx ' + (s3 ? '-m ' : '') + 'src/js/main build/main.js',
+                command: './node_modules/.bin/jspm bundle-sfx <%= visuals.jspmFlags %> src/js/main build/main.js',
                 options: {
                     execOptions: {
                         cwd: '.'
@@ -69,16 +56,20 @@ module.exports = function(grunt) {
         },
 
         'template': {
+            'options': {
+                'data': {
+                    'assetPath': '<%= visuals.assetPath %>',
+                }
+            },
             'harness': {
-                'options': {
-                    'data': {
-                        'assetPath': s3 ? s3target : '',
-                    }
-                },
                 'files': {
-                    'build/boot.js': ['harness/boot.js.tpl'],
                     'build/interactive.html': ['harness/interactive.html.tpl'],
                     'build/immersive.html': ['harness/immersive.html.tpl']
+                }
+            },
+            'bootjs': {
+                'files': {
+                    'build/boot.js': ['src/js/boot.js.tpl'],
                 }
             }
         },
@@ -91,11 +82,20 @@ module.exports = function(grunt) {
             },
             deploy: {
                 files: [
-                    {expand: true, cwd: 'build/', src: ['boot.js'], dest: 'deploy' },
-                    {expand: true, cwd: 'build/', src: ['main.js', 'main.css', 'main.js.map', 'main.css.map'], dest: 'deploy/' + timestamp }
+                    { // BOOT
+                        expand: true, cwd: 'build/',
+                        src: ['boot.js'],
+                        dest: 'deploy/<%= visuals.timestamp %>'
+                    },
+                    { // ASSETS
+                        expand: true, cwd: 'build/',
+                        src: ['main.js', 'main.css', 'main.js.map', 'main.css.map'],
+                        dest: 'deploy/<%= visuals.timestamp %>/<%= visuals.timestamp %>'
+                    }
                 ]
             }
         },
+
         symlink: {
             options: {
                 overwrite: false
@@ -104,6 +104,74 @@ module.exports = function(grunt) {
                 src: 'bower_components/guss-webfonts/webfonts',
                 dest: 'build/fonts/0.1.0'
             },
+        },
+
+        prompt: {
+            visuals: {
+                options: {
+                    questions: [
+                        {
+                            config: 'visuals.s3.stage',
+                            type: 'list',
+                            message: 'Deploy to TEST or PRODUCTION URL?',
+                            choices: [{
+                                name: 'TEST: <%= visuals.s3.domain %>testing/<%= visuals.s3.path %>',
+                                value: 'TEST'
+                            },{
+                                name: 'PROD: <%= visuals.s3.domain %><%= visuals.s3.path %>',
+                                value: 'PROD'
+                            }]
+                        },
+                        {
+                            config: 'visuals.confirmDeploy',
+                            type: 'confirm',
+                            message: 'Deploying to PRODUCTION. Are you sure?',
+                            default: false,
+                            when: function(answers) {
+                                return answers['visuals.s3.stage'] === 'PROD';
+                            }
+                        }
+                    ],
+                    then: function(answers) {
+                        if (grunt.config('visuals.s3.stage') !== 'PROD') { // first Q
+                            var prodPath = grunt.config('visuals.s3.path');
+                            var testPath = 'testing/' + prodPath;
+                            grunt.config('visuals.s3.path', testPath);
+                        } else if (answers['visuals.confirmDeploy'] !== true) { // second Q
+                            grunt.fail.warn('Please confirm to deploy to production.');
+                        }
+                    }
+                }
+            },
+        },
+
+        aws_s3: {
+            options: {
+                accessKeyId: '<%= visuals.aws.AWSAccessKeyID %>',
+                secretAccessKey: '<%= visuals.aws.AWSSecretKey %>',
+                region: 'us-east-1',
+                debug: grunt.option('dry'),
+                bucket: '<%= visuals.s3.bucket %>'
+            },
+            production: {
+                options: {
+                },
+                files: [
+                    { // ASSETS
+                        expand: true,
+                        cwd: 'deploy/<%= visuals.timestamp %>',
+                        src: ['<%= visuals.timestamp %>/**/*'],
+                        dest: '<%= visuals.s3.path %>',
+                        params: { CacheControl: 'max-age=2678400' }
+                    },
+                    { // BOOT
+                        expand: true,
+                        cwd: 'deploy/<%= visuals.timestamp %>',
+                        src: ['boot.js'],
+                        dest: '<%= visuals.s3.path %>',
+                        params: { CacheControl: 'max-age=60' }
+                    }]
+            }
         },
 
         connect: {
@@ -126,8 +194,28 @@ module.exports = function(grunt) {
         }
     });
 
+    grunt.registerTask('loadDeployConfig', function() {
+        if (!grunt.file.exists('cfg/aws-keys.json')) grunt.fail.fatal('./cfg/aws-keys.json missing');
+        grunt.config('visuals', {
+            s3: grunt.file.readJSON('./cfg/s3.json'),
+            aws: grunt.file.readJSON('./cfg/aws-keys.json'),
+            timestamp: Date.now(),
+            jspmFlags: '-m',
+            assetPath: '<%= visuals.s3.domain %><%= visuals.s3.path %>/<%= visuals.timestamp %>'
+        });
+    })
+
+    grunt.registerTask('boot_url', function() {
+        grunt.log.write('\nBOOT URL: '['green'].bold)
+        grunt.log.writeln(grunt.template.process('<%= visuals.s3.domain %><%= visuals.s3.path %>/boot.js'))
+    })
+
     grunt.registerTask('harness', ['copy:harness', 'template:harness', 'sass:harness', 'symlink:fonts'])
-    grunt.registerTask('interactive', ['shell:interactive', 'sass:interactive'])
+    grunt.registerTask('interactive', ['shell:interactive', 'template:bootjs', 'sass:interactive'])
     grunt.registerTask('default', ['clean', 'harness', 'interactive', 'connect', 'watch']);
-    grunt.registerTask('build', ['clean', 'interactive', 'copy:deploy']);
+    grunt.registerTask('build', ['clean', 'interactive']);
+    grunt.registerTask('deploy', ['loadDeployConfig', 'prompt:visuals', 'build', 'copy:deploy', 'aws_s3', 'boot_url']);
+
+    grunt.loadNpmTasks('grunt-aws');
+
 }
