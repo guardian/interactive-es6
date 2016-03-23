5517ff825b52dcb4fc4641efe549e8d81cb547e0
Matt Osborn <mattjosborn@gmail.com>
2016-03-23 12:41:00 +0000

    update share library

--- src/js/lib/share.js
+++ src/js/lib/share.js
@@ -1,16 +1,18 @@
 const twitterBaseUrl = 'https://twitter.com/intent/tweet?text=';
-const facebookBaseUrl = 'https://www.facebook.com/sharer/sharer.php?ref=responsive&u=';
+const facebookBaseUrl = 'https://www.facebook.com/dialog/feed?display=popup&app_id=741666719251986&redirect_uri=http://www.theguardian.com&link=';
 const googleBaseUrl = 'https://plus.google.com/share?url=';
 
-export default function share(title, shareURL, hashTag) {
+export default function share(title, shareURL, fbImg, twImg, hashTag) {
+    var twImgText = twImg ? ` ${twImg.trim()} ` : ' ';
+    var fbImgQry = fbImg ? `&picture=${encodeURIComponent(fbImg)}` : '';
     return function (network, extra='') {
-        var twitterMessage = `${extra}${title} ${hashTag}`;
+        var twitterMessage = `${extra}${title}${twImgText}${hashTag}`;
         var shareWindow;
 
         if (network === 'twitter') {
             shareWindow = twitterBaseUrl + encodeURIComponent(twitterMessage + ' ') + shareURL;
         } else if (network === 'facebook') {
-            shareWindow = facebookBaseUrl + shareURL;
+            shareWindow = facebookBaseUrl + shareURL + fbImgQry;
         } else if (network === 'email') {
             shareWindow = 'mailto:?subject=' + encodeURIComponent(title) + '&body=' + shareURL;
         } else if (network === 'google') {
