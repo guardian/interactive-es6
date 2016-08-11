cd051e7f55bc25affdc3082709afacf89c8c98fa
Gideon Goldberg <gidsgoldberg@gmail.com>
2016-08-11 12:16:38 +0100

    use youtube-iframe lib

--- src/js/lib/youtube.js
+++ src/js/lib/youtube.js
@@ -1,5 +1,6 @@
+import youTubeIframe from 'youtube-iframe'
+
 export default function pimpYouTubePlayer(videoId, placeholderId) {
-    var player;
 
     function getPlayerProps() {
         const o = {};
@@ -14,34 +15,25 @@ export default function pimpYouTubePlayer(videoId, placeholderId) {
     }
 
 
-
-
-    // 2. This code loads the IFrame Player API code asynchronously.
-    var tag = document.createElement('script');
-
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
     function onPlayerReady(event) {
         p.style.display = 'none';
         event.target.playVideo();
     }
-
-
+    
 
     const p = document.getElementById('placeholder');
     p.addEventListener('click', function() {
-        player = new YT.Player(placeholderId, {height: '390',
-            width: '640',
-            videoId: videoId,
-            enablejsapi: 1,
-            events: {
-                onReady: onPlayerReady
-            },
-            playerVars: getPlayerProps()
+        youTubeIframe.load(function(YT) {
+            new YT.Player(placeholderId, {
+                height: '390',
+                width: '640',
+                videoId: videoId,
+                enablejsapi: 1,
+                events: {
+                    onReady: onPlayerReady
+                },
+                playerVars: getPlayerProps()
+            });
         });
     });
-
-
-
 }
